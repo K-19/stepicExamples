3be8e2ee08ece1621aca167185325008005d033e
K-19 <filinovich.sergey.work@gmail.com>
2021-09-20 23:27:22 +0300

    create class Thief

---
+++ src/main/java/by/k19/posts/Thief.java
@@ -0,0 +1,14 @@
+package by.k19.posts;
+
+public class Thief {
+    private int minValue;
+    private int stolenValue;
+
+    public Thief(int minValue) {
+        this.minValue = minValue;
+    }
+
+    public int getStolenValue() {
+        return stolenValue;
+    }
+}
