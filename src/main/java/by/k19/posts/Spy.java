89ee9900e3e5e9d8c02e319174583ae412077249
K-19 <filinovich.sergey.work@gmail.com>
2021-09-20 23:25:05 +0300

    create class Spy

---
+++ src/main/java/by/k19/posts/Spy.java
@@ -0,0 +1,11 @@
+package by.k19.posts;
+
+import java.util.logging.Logger;
+
+public class Spy {
+    private Logger logger;
+
+    public Spy(Logger logger) {
+        this.logger = logger;
+    }
+}
