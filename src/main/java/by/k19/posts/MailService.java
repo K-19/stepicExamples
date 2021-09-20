398a475ffd097adcad1f04dd352fa20ac83b249a
K-19 <filinovich.sergey.work@gmail.com>
2021-09-20 23:14:59 +0300

    initial of task about post (init package by.k19.post)

---
+++ src/main/java/by/k19/posts/MailService.java
@@ -0,0 +1,5 @@
+package by.k19.posts;
+
+public static interface MailService {
+    Sendable processMail(Sendable mail);
+}
---
+++ src/main/java/by/k19/posts/RealMailService.java
@@ -0,0 +1,10 @@
+package by.k19.posts;
+
+public static class RealMailService implements MailService {
+
+    @Override
+    public Sendable processMail(Sendable mail) {
+        // Здесь описан код настоящей системы отправки почты.
+        return mail;
+    }
+}
