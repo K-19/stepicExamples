37d9379c2106382c420d46b8e79ee70e3e0ab8ee
K-19 <filinovich.sergey.work@gmail.com>
2021-09-20 23:21:19 +0300

    create class UntrustworthyMailWorker

---
+++ src/main/java/by/k19/posts/UntrustworthyMailWorker.java
@@ -0,0 +1,10 @@
+package by.k19.posts;
+
+public class UntrustworthyMailWorker {
+    private RealMailService realMailService;
+
+
+    public UntrustworthyMailWorker(MailService mailService) {
+
+    }
+}
