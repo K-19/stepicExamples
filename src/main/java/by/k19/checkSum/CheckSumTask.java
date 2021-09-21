c6569d5cea8a821fca5e541a93985897c881bc65
K-19 <filinovich.sergey.work@gmail.com>
2021-09-21 23:17:06 +0300

    complete Task with check Sum

--- src/main/java/by/k19/checkSum/CheckSumTask.java
+++ src/main/java/by/k19/checkSum/CheckSumTask.java
@@ -6,13 +6,18 @@ import java.io.InputStream;
 
 public class CheckSumTask {
     public static void main(String[] args) throws IOException {
-        byte[] array = {3,45,6,6,76,5,-100,35,6,57,100,34,53,4,2,3,5};
+        byte[] array = {0x33, 0x45, 0x01};
         try (ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(array)) {
             System.out.println(checkSumOfStream(byteArrayInputStream));
         }
     }
 
     public static int checkSumOfStream(InputStream inputStream) throws IOException {
-
+        int currentByte;
+        int sum = 0;
+        while((currentByte = inputStream.read()) != -1) {
+            sum = Integer.rotateLeft(sum, 1) ^ currentByte;
+        }
+        return sum;
     }
 }
