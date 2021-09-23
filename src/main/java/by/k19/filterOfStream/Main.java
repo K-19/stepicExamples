c93581aef891621706b65fe9662b9227b38fdee4
K-19 <filinovich.sergey.work@gmail.com>
2021-09-23 15:49:33 +0300

    first try to complete the task about filter of stream (no success)

--- src/main/java/by/k19/filterOfStream/Main.java
+++ src/main/java/by/k19/filterOfStream/Main.java
@@ -1,4 +1,26 @@
 package by.k19.filterOfStream;
 
+import java.io.IOException;
+
 public class Main {
+    public static void main(String[] args) {
+        try {
+            int next, prev = System.in.read();
+            while(prev != -1) {
+                next = System.in.read();
+                if (prev == 13 || next == 10)
+                    prev = next;
+                else {
+                    System.out.write(prev);
+                    prev = next;
+                }
+            }
+            if (prev != -1) {
+                System.out.write(prev);
+            }
+        } catch (IOException ignored) {
+        } finally {
+            System.out.flush();
+        }
+    }
 }
