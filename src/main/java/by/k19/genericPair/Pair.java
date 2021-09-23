d1db9005efbaf10d5ad9cafd625d85919464e4eb
K-19 <filinovich.sergey.work@gmail.com>
2021-09-23 22:12:44 +0300

    complete generic Pair task

--- src/main/java/by/k19/genericPair/Pair.java
+++ src/main/java/by/k19/genericPair/Pair.java
@@ -1,6 +1,8 @@
 package by.k19.genericPair;
 
-public class Pair {
+import java.util.*;
+
+public class Pair<F, S> {
     public static void main(String[] args) {
         Pair<Integer, String> pair = Pair.of(1, "hello");
         Integer i = pair.getFirst(); // 1
@@ -9,5 +11,42 @@ public class Pair {
         Pair<Integer, String> pair2 = Pair.of(1, "hello");
         boolean mustBeTrue = pair.equals(pair2); // true!
         boolean mustAlsoBeTrue = pair.hashCode() == pair2.hashCode(); // true!
+
+//        Optional<String> opt = Optional.of("ha");
+    }
+
+    private F first;
+    private S second;
+
+    private Pair() {}
+
+    public Pair(F first, S second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public F getFirst() {
+        return first;
+    }
+
+    public S getSecond() {
+        return second;
+    }
+
+    public static <F, S> Pair<F, S> of(F first, S second) {
+        return new Pair<>(first, second);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Pair)) return false;
+        Pair<?, ?> pair = (Pair<?, ?>) o;
+        return Objects.equals(first, pair.first) && Objects.equals(second, pair.second);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(first, second);
     }
 }
