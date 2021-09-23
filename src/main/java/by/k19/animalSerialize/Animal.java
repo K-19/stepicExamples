cc3ad44338ab2b7d03c8bc5ab7500e6756da6f3e
K-19 <filinovich.sergey.work@gmail.com>
2021-09-23 17:54:52 +0300

    complete task

--- src/main/java/by/k19/animalSerialize/Animal.java
+++ src/main/java/by/k19/animalSerialize/Animal.java
@@ -1,6 +1,9 @@
 package by.k19.animalSerialize;
 
-import java.io.Serializable;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Objects;
 
 class Animal implements Serializable {
@@ -21,5 +24,41 @@ class Animal implements Serializable {
     @Override
     public int hashCode() {
         return Objects.hash(name);
+    }
+
+    private static final String FILE_NAME = "animals.txt";
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        Animal[] animals = new Animal[]{
+                new Animal("Elephant"),
+                new Animal("Cat"),
+                new Animal("Pig"),
+                new Animal("Shark")};
+        ObjectOutput oos = new ObjectOutputStream(Files.newOutputStream(Paths.get(FILE_NAME)));
+        oos.writeInt(animals.length);
+        for (Animal animal : animals) {
+            oos.writeObject(animal);
+        }
+        oos.close();
+
+        ObjectInputStream ois = new ObjectInputStream(Files.newInputStream(Paths.get(FILE_NAME)));
+        int sizeAnimals = ois.readInt();
+        for (int i = 0; i < sizeAnimals; i++) {
+            System.out.println((Animal)ois.readObject());
+        }
+    }
+
+    public static Animal[] deserializeAnimalArray(byte[] data) throws IllegalArgumentException {
+        try {
+            ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(data));
+            int sizeAnimals = ois.readInt();
+            Animal[] animals = new Animal[sizeAnimals];
+            for (int i = 0; i < sizeAnimals; i++) {
+                animals[i] = (Animal) ois.readObject();
+            }
+            return animals;
+        } catch (Exception ignored) {
+            throw new IllegalArgumentException();
+        }
     }
 }
