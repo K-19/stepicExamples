23bb8b95eea510a19cc6ac8257e8162074d970e5
K-19 <filinovich.sergey.work@gmail.com>
2021-09-27 16:50:54 +0300

    init FindMinMax

---
+++ src/main/java/by/k19/smallTasks/FindMinMax.java
@@ -0,0 +1,20 @@
+package by.k19.smallTasks;
+
+import java.util.Comparator;
+import java.util.function.BiConsumer;
+import java.util.stream.Stream;
+
+public class FindMinMax {
+    public static <T> void findMinMax(
+            Stream<? extends T> stream,
+            Comparator<? super T> order,
+            BiConsumer<? super T, ? super T> minMaxConsumer) {
+
+
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
