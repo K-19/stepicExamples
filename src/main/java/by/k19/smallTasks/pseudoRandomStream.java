c60ce5b32db451234c3c3aad6b888fceee4e08d9
K-19 <filinovich.sergey.work@gmail.com>
2021-09-27 16:37:20 +0300

    complete pseudoRandomStream

--- src/main/java/by/k19/smallTasks/pseudoRandomStream.java
+++ src/main/java/by/k19/smallTasks/pseudoRandomStream.java
@@ -1,13 +1,14 @@
 package by.k19.smallTasks;
 
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class pseudoRandomStream {
     public static IntStream pseudoRandomStream(int seed) {
-        return null; // your implementation here
+        return IntStream.iterate(seed, n -> (n * n) % 10000 / 10);
     }
 
     public static void main(String[] args) {
-
+        pseudoRandomStream(13).limit(20).forEach(System.out::println);
     }
 }
